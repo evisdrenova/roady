c46650ed3e31004aed24a5f135438cbb94aa9923
Evis Drenova <evis@usenucleus.cloud>
2024-08-08 13:54:31 -0700

    add favicon

--- components/TopNavBar.tsx
+++ components/TopNavBar.tsx
@@ -4,6 +4,7 @@ import GoogleOAuth from "./GoogleOAuth";
 import UserProfile from "./UserProfile";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 interface Props {
   openOAuth: boolean;
@@ -13,11 +14,24 @@ interface Props {
 export default function TopNavBar(props: Props): ReactElement {
   const { openOAuth, setOpenOAuth } = props;
 
+  const theme = useTheme();
+
   return (
     <div className="flex flex-row justify-between items-center gap-2">
-      <Link href="/">
-        <Image src="/roady_logo.svg" width="24" height="24" alt="logo" />
-      </Link>
+      {theme.theme == "light" ? (
+        <Link href="/">
+          <Image
+            src="/roady_logo_light.svg"
+            width="24"
+            height="24"
+            alt="logo"
+          />
+        </Link>
+      ) : (
+        <Link href="/">
+          <Image src="/roady_logo_dark.svg" width="24" height="24" alt="logo" />
+        </Link>
+      )}
       <div className="flex flex-row gap-1">
         <ModeToggle />
         <GoogleOAuth setOpenOAuth={setOpenOAuth} openOAuth={openOAuth} />
